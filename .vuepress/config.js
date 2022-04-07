16bc3129e6f1926ef0a4b4b1df65042aff15538b
Fahviz <55505992+Fahviz@users.noreply.github.com>
2022-04-07 08:23:24 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
   title: "VuePress 云开发网站应用示例",
   description: "基于云开发的 VuePress 网站应用示例",
-  base: "/vuepress/",
+  base: "/",
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
       { text: "指南", link: "/guides/" },
-      { text: "我的导航", link: "/links"},
     ],
   },
 };
